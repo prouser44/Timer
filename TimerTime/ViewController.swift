6ad30777b24f172bac4cfbedb254b1124dbab9bd
E.Piper <spagetboi@SCE18-07.local>
2019-12-18 13:22:39 -0500

    testing

---
+++ TimerTime/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TimerTime
+//
+//  Created by E.Piper on 12/17/19.
+//  Copyright © 2019 E.Piper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+//did the push work?
+    //v2
+}
+
